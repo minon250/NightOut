1445e8ba893014bf599c55fa39498e1935477b88
minon250 <minon250@gmail.com>
2017-04-16 11:10:40 +1000

    added DB Controller

---
+++ NightOut/NightOut/FirebaseDataController.swift
@@ -0,0 +1,14 @@
+//
+//  FirebaseDataController.swift
+//  NightOut
+//
+//  Created by Minon Weerasinghe on 16/4/17.
+//  Copyright © 2017 Minon Weerasinghe. All rights reserved.
+//
+
+import Foundation
+
+class FirebaseDataController {
+    // This class will handle all Firebase data controlls
+}
+
