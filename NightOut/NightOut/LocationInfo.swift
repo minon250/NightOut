4dbf0a086de4d9c32082777ce27552d16a4657b6
minon250 <minon250@gmail.com>
2017-04-18 09:24:53 +1000

    add button modification class aand working on firebase retrieval

---
+++ NightOut/NightOut/LocationInfo.swift
@@ -0,0 +1,9 @@
+//
+//  LocationInfo.swift
+//  NightOut
+//
+//  Created by Minon Weerasinghe on 17/4/17.
+//  Copyright © 2017 Minon Weerasinghe. All rights reserved.
+//
+
+import Foundation
---
+++ NightOut/NightOut/RoundedButtons.swift
@@ -0,0 +1,21 @@
+//
+//  RoundedButtons.swift
+//  NightOut
+//
+//  Created by Minon Weerasinghe on 17/4/17.
+//  Copyright © 2017 Minon Weerasinghe. All rights reserved.
+//
+
+import UIKit
+
+class RoundedButtons: UIButton {
+
+    /*
+    // Only override draw() if you perform custom drawing.
+    // An empty implementation adversely affects performance during animation.
+    override func draw(_ rect: CGRect) {
+        // Drawing code
+    }
+    */
+
+}
