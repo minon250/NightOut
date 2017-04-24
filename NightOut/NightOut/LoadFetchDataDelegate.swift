1ab9bb017fba1d53c5f8166cfd3cc47358ff950a
minon250 <minon250@gmail.com>
2017-04-24 21:38:02 +1000

    implemented the delegate for table view and firebase

---
+++ NightOut/NightOut/LoadFetchDataDelegate.swift
@@ -0,0 +1,9 @@
+//
+//  LoadFetchDataDelegate.swift
+//  NightOut
+//
+//  Created by Minon Weerasinghe on 24/4/17.
+//  Copyright © 2017 Minon Weerasinghe. All rights reserved.
+//
+
+import Foundation
