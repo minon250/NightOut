40bf9223d98bee34ef329336fdeb14aa2a6c4ca4
minon250 <minon250@gmail.com>
2017-04-29 14:23:24 +1000

    Table view design updated

---
+++ NightOut/NightOut/CardViewTableCell.swift
@@ -0,0 +1,24 @@
+//
+//  CardViewTableCell.swift
+//  NightOut
+//
+//  Created by Minon Weerasinghe on 26/4/17.
+//  Copyright © 2017 Minon Weerasinghe. All rights reserved.
+//
+
+import UIKit
+
+class CardViewTableCell: UITableViewCell {
+
+    override func awakeFromNib() {
+        super.awakeFromNib()
+        // Initialization code
+    }
+
+    override func setSelected(_ selected: Bool, animated: Bool) {
+        super.setSelected(selected, animated: animated)
+
+        // Configure the view for the selected state
+    }
+    
+}
